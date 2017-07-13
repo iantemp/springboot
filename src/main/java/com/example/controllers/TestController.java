ec4923006836e915fe59aa821a500c13936b2d20
Joseph Gonzales <josephgonzales01@gmail.com>
2017-07-13 16:05:56 +0800

    added oracle config

--- src/main/java/com/example/controllers/TestController.java
+++ src/main/java/com/example/controllers/TestController.java
@@ -1,15 +1,18 @@
 package com.example.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Ian Gabriel on 7/13/2017.
  */
+@RestController
 public class TestController {
 
-    @RequestMapping("/welcome.html")
+    @RequestMapping("/welcome")
     public ModelAndView firstPage(){
-        return new ModelAndView("welcome");
+        return new ModelAndView("welcome.html");
     }
 }
